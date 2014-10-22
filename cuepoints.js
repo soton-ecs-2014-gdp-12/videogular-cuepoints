1b73eff292a50db7502836b1ccfb810b2e45c803
Harry Cutts <eternal.linux@gmail.com>
2014-10-22 14:39:19 +0100

    Display red bars at points on the scrub bar

---
+++ cuepoints.js
@@ -0,0 +1,33 @@
+(function(){
+'use strict';
+angular.module('uk.ac.soton.ecs.videogular.plugins.cuepoints', [])
+	.directive(
+		'vgCuepoints',
+		[function() {
+			return {
+				restrict: 'E',
+				require: '^videogular',
+				templateUrl: 'bower_components/videogular-cuepoints/cuepoints.html',
+				scope: {
+					cuepoints: '=vgCuepointsConfig',
+					theme: '=vgCuepointsTheme',
+				},
+				link: function($scope, elem, attr, API) {
+					// shamelessly stolen from part of videogular's updateTheme function
+					function updateTheme(value) {
+						if (value) {
+							var headElem = angular.element(document).find("head");
+							headElem.append("<link rel='stylesheet' href='" + value + "'>");
+						}
+					}
+
+					$scope.calcLeft = function(cuepoint) {
+						var videoLength = API.totalTime.getTime() / 1000;
+						return (cuepoint.time * 100 / videoLength).toString();
+					};
+
+					updateTheme($scope.theme);
+				},
+			};
+		}]);
+})();
